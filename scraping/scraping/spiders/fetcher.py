562525be09ec1058ffee6510730dd3b64673710e
AndreaMercanti <andrea.mr@hotmail.it>
2020-05-20 18:25:08 +0200

    Modify the request method and other minor features
    Upgrade the start_requests method to loop on each film in the dataset.
    
    Add two instance variables for tracking the movie the rieviews are referring to.
    
    Switch the date string with a date object to allow a better manipualtion in later work.

--- scraping/scraping/spiders/fetcher.py
+++ scraping/scraping/spiders/fetcher.py
@@ -2,6 +2,7 @@
 import scrapy
 from ..items import ScrapingItem
 from datetime import datetime
+import json
 
 class FetcherSpider(scrapy.Spider):
     name = 'fetcher'
@@ -10,11 +11,20 @@ class FetcherSpider(scrapy.Spider):
     def __init__(self, **kwargs):
         if not hasattr(self, 'ajax_url'):
             self.ajax_url = None # instance variable
+        if not hasattr(self, 'film_id'):
+            self.film_id = None # instance variable
+        if not hasattr(self, 'film_title'):
+            self.film_title = None # instance variable
         super().__init__(**kwargs)
 
     def start_requests(self):
-        self.ajax_url = 'https://www.imdb.com/title/tt0113497/reviews/_ajax?sort=userRating&dir=desc&ratingFilter=0'
-        yield scrapy.Request(self.ajax_url, callback=self.parse, dont_filter=True)
+        with open('movie_dataset.json', 'r') as file:
+            for line in file.readlines():
+                movie = json.loads(line)
+                self.film_id = movie['imdbID']
+                self.film_title = movie['Title']
+                self.ajax_url = 'https://www.imdb.com/title/{}/reviews/_ajax?sort=userRating&dir=desc&ratingFilter=0'.format(self.film_id)
+                yield scrapy.Request(self.ajax_url, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
         item = ScrapingItem()
@@ -27,10 +37,12 @@ class FetcherSpider(scrapy.Spider):
             review_components = review.css(".show-more__control::text").extract()
 
             # PARSING AND BETTER FORMATTING THE DIFFERENT ELEMS
-            date = str(datetime.strptime(date, '%d %B %Y').date()) # formatting in a 'YYYY-MM-DD'-like string
-            review_str = ''.join(review_components[0:-2]) # compatting all review components into one string, but the last two
+            date = datetime.strptime(date, '%d %B %Y').date() # creating a date object
+            review_str = ''.join(review_components[0:-2]) # compatting all the review components into one string, but the last two
             rating = ''.join(rating_components) # compatting all rating components into one string
 
+            item['filmID'] = self.film_id
+            item['filmTitle'] = self.film_title
             item['user'] = user
             item['rating'] = rating
             item['date'] = date
